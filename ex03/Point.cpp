df8e8359bd9c68f212109716a8167f59f4604bc6
jaiveca- <jaiveca-@student.42lisboa.com>
2023-07-07 04:30:59 +0100

    advancing on ex03

--- ex03/Point.cpp
+++ ex03/Point.cpp
@@ -6,7 +6,7 @@
 /*   By: jaiveca- <jaiveca-@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 13:14:27 by jaiveca-          #+#    #+#             */
-/*   Updated: 2023/07/07 02:55:41 by jaiveca-         ###   ########.fr       */
+/*   Updated: 2023/07/07 03:51:54 by jaiveca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,10 @@ Point::Point(): _x(0), _y(0){}
 
 Point::Point(float x, float y): _x(x), _y(y){}
 
+Point::Point(const Point &point): _x(point.getX()), _y(point.getY()){}
 
+Point	&Point::operator=(const Point &point)
+{
+	return (*this);
+}
 
